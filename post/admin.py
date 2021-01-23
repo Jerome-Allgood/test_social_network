dc42dfd69bf190b30b013953df8f825cf501e2f4
Mykola Moskvin <nsmoskvin@gmail.com>
2021-01-23 12:17:37 +0200

    Post model added. Post model added to Admin panel. Migration needed.

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from post.models import Post
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+	list_display = ('id', 'author', 'body', 'created_at', 'updated_at')
+	search_fields = ('author', 'body', 'id')
+	ordering = ('created_at',)
--- post/models.py
+++ post/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from register.models import User
+
+
+class Post(models.Model):
+	author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
+	body = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True, null=True)
+	updated_at = models.DateTimeField(auto_now=True, null=True)
+
+	def __str__(self):
+		return f'Post id {self.id} by {self.author}'
