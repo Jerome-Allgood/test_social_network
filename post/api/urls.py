ab7701a9470b47057d7cd6e259a4227f9361c805
Mykola Moskvin <nsmoskvin@gmail.com>
2021-01-24 17:48:28 +0200

    Basic Post list view added.

--- post/api/urls.py
+++ post/api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from post.api.views import CreatePostView
+from post.api.views import CreatePostView, ListPostView
 
 urlpatterns = [
     path('post/create/', CreatePostView.as_view()),
+    path('post/list/', ListPostView.as_view()),
+
 ]
--- post/api/views.py
+++ post/api/views.py
@@ -1,9 +1,16 @@
 from rest_framework import permissions
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView
 
 from post.api.serializers import PostSerializer
+from post.models import Post
 
 
 class CreatePostView(CreateAPIView):
 	permission_classes = [permissions.IsAuthenticated,]
 	serializer_class = PostSerializer
+
+
+class ListPostView(ListAPIView):
+	permission_classes = [permissions.IsAuthenticated,]
+	serializer_class = PostSerializer
+	queryset = Post.objects.all()
