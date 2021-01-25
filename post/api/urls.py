2af0662c9b27495639446d7f49197154adbe179f
Mykola Moskvin <nsmoskvin@gmail.com>
2021-01-25 22:06:21 +0200

    URL names added.

--- post/api/urls.py
+++ post/api/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from post.api.views import CreatePostView, ListPostView, LikePostView
 
 urlpatterns = [
-    path('post/create/', CreatePostView.as_view()),
-    path('post/list/', ListPostView.as_view()),
-    path('post/<int:pk>/like/', LikePostView.as_view()),
+    path('post/create/', CreatePostView.as_view(), name='create_post'),
+    path('post/list/', ListPostView.as_view(), name='list_post'),
+    path('post/<int:pk>/like/', LikePostView.as_view(), name='like_post'),
 
 ]
--- register/api/urls.py
+++ register/api/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from register.api.views import CreateUserView, LoginView, LogoutView
 
 urlpatterns = [
-    path('users/register/', CreateUserView.as_view()),
-    path('users/login/', LoginView.as_view()),
-    path('users/logout/', LogoutView.as_view()),
+    path('users/register/', CreateUserView.as_view(), name='register'),
+    path('users/login/', LoginView.as_view(), name='login'),
+    path('users/logout/', LogoutView.as_view(), name='logout'),
 
 ]
