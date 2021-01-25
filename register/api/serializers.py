ad6e0ac8d0a92912025d9861861f4bb252d1a1f2
Mykola Moskvin <nsmoskvin@gmail.com>
2021-01-25 22:50:38 +0200

    Email validation environment variable dependency added to prevent exciding limits on third-party API.

--- register/api/serializers.py
+++ register/api/serializers.py
@@ -19,6 +19,8 @@ class UserSerializer(serializers.ModelSerializer):
 		read_only_fields = ('id',)
 
 	def validate_email(self, value):
+		if settings.DEMONSTRATION:
+			return value
 		response = requests.get(_get_email_verification_url(value))
 		if (response.status_code == status.HTTP_400_BAD_REQUEST or
 				response.json()['data']['status'] in ['invalid', 'unknown']):
